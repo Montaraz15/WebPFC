f494f931c2868d872646031e19eeebd8ed1866f0
Gonzalo <gonmurmor@gmail.com>
2015-08-11 18:25:47 +0200

    Se añaden los archivos que deben estar en el servidor.

---
+++ serverfiles/config.php
@@ -0,0 +1,7 @@
+<?php
+ // Database configuration
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '28833198f');
+    define('DB_HOST', '127.0.0.1');
+    define('DB_NAME', 'wcdb');
+?>
