2fe064001713ef0c88e40e1af673761712932b7d
Gonzalo <gonmurmor@gmail.com>
2015-08-24 19:02:12 +0200

    Eliminacion de archivos de server

--- serverfiles/config.php
+++
@@ -1,7 +0,0 @@
-<?php
- // Database configuration
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '28833198f');
-    define('DB_HOST', '127.0.0.1');
-    define('DB_NAME', 'wcdb');
-?>
